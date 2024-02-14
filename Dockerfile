349b8d289ddfdc490b92429127ea13057c682e47
Roy de Wildt <roydewildt@gmail.com>
2024-02-14 23:47:55 +0100

    refactored images

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ ARG GO_VERSION=1.22
 
 FROM golang:${GO_VERSION}-alpine as builder
 WORKDIR /usr/src/app
-COPY go.mod go.sum ./
-RUN go mod download && go mod verify
 COPY . .
 RUN go build -v -o /run-app .
 
--- main.go
+++ main.go
@@ -8,12 +8,16 @@ import (
 	"os"
 )
 
-//go:embed favicon.ico logo.svg
-var assets embed.FS
+var (
+	port = "8080"
+
+	//go:embed assets
+	assets embed.FS
+)
 
 func main() {
-	http.Handle("GET /logo.svg", http.FileServerFS(assets))
-	http.Handle("GET /favicon.ico", http.FileServerFS(assets))
+	http.Handle("GET /assets/", http.FileServerFS(assets))
+
 	http.HandleFunc("GET /", func(w http.ResponseWriter, _ *http.Request) {
 		slog.Info("new request")
 		t, err := template.New("landing").Parse(landing)
@@ -24,8 +28,9 @@ func main() {
 		t.Execute(w, nil)
 	})
 
-	slog.Info("server started...", "port", 8080)
-	if err := http.ListenAndServe(":8080", http.DefaultServeMux); err != nil {
+	slog.Info("server started...", "port", port)
+
+	if err := http.ListenAndServe(":"+port, http.DefaultServeMux); err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
@@ -37,17 +42,32 @@ const landing = `
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Creating digital planners, digital stickers, and digital notebooks for platforms like GoodNotes" />
+
+    <meta property="og:title" content="Four Rabbits Studio" />
+    <meta property="og:url" content="https://www.fourrabbitsstudio.com/" />
+    <meta property="og:description" content="Creating digital planners, digital stickers, and digital notebooks for platforms like GoodNotes" />
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="en_GB" />
+
+    <meta property="og:image" content="http://www.fourrabbitsstudio.com/assets/logo.png" />
+    <meta property="og:image:secure_url" content="http://www.fourrabbitsstudio.com/assets/logo.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="300" />
+    <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
+
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Four Rabbits Studio</title>
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
   </head>
   <body style="margin: 0 2rem; height: 100%;">
   <main style="height: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
-        <img style="max-width: 100%;"src="logo.svg" alt="logo">
+        <img style="max-width: 100%;"src="/assets/logo.svg" alt="logo">
         <div style="margin: 6rem 0; display: flex; flex-direction: row; justify-content: center; align-items: center; gap: 4rem;">
-            <a href="https://www.etsy.com/"><svg height=50 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M384 348c-1.8 10.8-13.8 110-15.5 132-117.9-4.3-219.9-4.7-368.5 0v-25.5c45.5-8.9 60.6-8 61-35.3 1.8-72.3 3.5-244.1 0-322-1-28.5-12.1-26.8-61-36v-25.5c73.9 2.4 255.9 8.6 363-3.8-3.5 38.3-7.8 126.5-7.8 126.5H332C320.9 115.7 313.2 68 277.3 68h-137c-10.3 0-10.8 3.5-10.8 9.8V241.5c58 .5 88.5-2.5 88.5-2.5 29.8-1 27.6-8.5 40.8-65.3h25.8c-4.4 101.4-3.9 61.8-1.8 160.3H257c-9.2-40.1-9.1-61-39.5-61.5 0 0-21.5-2-88-2v139c0 26 14.3 38.3 44.3 38.3H263c63.6 0 66.6-25 98.8-99.8H384z"/></svg></a>
-            <a href="https://www.instagram.com/"><svg height=50 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg></a>
-            <a href="https://www.tiktok.com/"><svg height=50 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"/></svg></a>
+            <a href="https://www.etsy.com/"><img style="height: 3rem;" src="/assets/etsy.svg" alt="etsy"></img></a>
+            <a href="https://www.instagram.com/"><img style="height: 3rem;" src="/assets/instagram.svg" alt="instagram"></img></a>
+            <a href="https://www.tiktok.com/"><img style="height: 3rem;" src="/assets/tiktok.svg" alt="tiktok"></img></a>
         </div>
     </main>
   </body>

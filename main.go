067db5599b3e02e51fa3ead6bd6c3dc8002cd178
Roy de Wildt <roydewildt@gmail.com>
2024-02-15 09:42:13 +0100

    embed templates

--- main.go
+++ main.go
@@ -13,19 +13,22 @@ var (
 
 	//go:embed assets
 	assets embed.FS
+
+	//go:embed *.tmpl
+	templates embed.FS
 )
 
 func main() {
 	http.Handle("GET /assets/", http.FileServerFS(assets))
 
-	t, err := template.ParseFiles("index.html.tmpl")
+	t, err := template.ParseFS(templates, "*.tmpl")
 	if err != nil {
 		slog.Error(err.Error())
 	}
 
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("new request", "url", r.URL)
-		if err := t.Execute(w, nil); err != nil {
+		if err := t.ExecuteTemplate(w, "root.html.tmpl", nil); err != nil {
 			slog.Error(err.Error())
 			http.Error(w, "Whoeps!", http.StatusInternalServerError)
 		}
